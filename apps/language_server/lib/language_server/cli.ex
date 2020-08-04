d3034b2c946ce3519410a70ca736d709559b77c3
Jason Axelson <jason.axelson@gmail.com>
2020-08-04 07:50:48 -1000

    add clustering and update version

--- apps/language_server/lib/language_server/cli.ex
+++ apps/language_server/lib/language_server/cli.ex
@@ -10,10 +10,40 @@ defmodule ElixirLS.LanguageServer.CLI do
     IO.puts("Started ElixirLS v#{Launch.language_server_version()}")
     Launch.print_versions()
 
+    start_node()
+
     Mix.shell(ElixirLS.LanguageServer.MixShell)
     # FIXME: Private API
     Mix.Hex.ensure_updated?()
 
     WireProtocol.stream_packets(&JsonRpc.receive_packet/1)
+  end
+
+  def start_node(number \\ 0) do
+    node_name = node_name(number)
+
+    case Node.start(node_name, :shortnames) do
+      {:error, _error} ->
+        start_node(number + 1)
+
+      {:ok, _pid} ->
+        IO.puts(
+          """
+          Started node with name #{inspect(node_name)}. Connect by running:
+          `iex --sname local --cookie cookie`
+          Then within iex run `Node.connect(#{inspect(node_name)})`
+          Now you can run `:observer.start()` and switch to the #{inspect(node_name)} and analyze ElixirLS
+          """
+        )
+
+        Node.set_cookie(node_name, :cookie)
+    end
+  end
+
+  def node_name(number) do
+    {:ok, hostname} = :inet.gethostname()
+
+    "elixirls-#{number}@#{hostname}"
+    |> String.to_atom()
   end
 end
